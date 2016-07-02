454a894abcdce2274279d1cd50a4b1dffa49c5df
Alex Basson <alexbasson@gmail.com>
2016-07-02 16:15:44 -0400

    Initial Commit

---
+++ Whiteboard/Whiteboard.h
@@ -0,0 +1,19 @@
+//
+//  Whiteboard.h
+//  Whiteboard
+//
+//  Created by Alex on 7/2/16.
+//  Copyright © 2016 Conditionally Convergent. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Whiteboard.
+FOUNDATION_EXPORT double WhiteboardVersionNumber;
+
+//! Project version string for Whiteboard.
+FOUNDATION_EXPORT const unsigned char WhiteboardVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Whiteboard/PublicHeader.h>
+
+
