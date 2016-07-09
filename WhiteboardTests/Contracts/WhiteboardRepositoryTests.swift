025082cfc83d88adda93fb56747e1d349f129bc0
Alex Basson <alexbasson@gmail.com>
2016-07-08 21:29:35 -0400

    Move WhiteboardRepositoryTests into Contracts group

---
+++ WhiteboardTests/Contracts/WhiteboardRepositoryTests.swift
@@ -0,0 +1,49 @@
+import XCTest
+
+class WhiteboardRepositoryTests: XCTestCase {
+  var repo: WhiteboardRepository?
+
+  let ny = Whiteboard(name: "NY")
+  let sf = Whiteboard(name: "SF")
+
+  override func setUp() {
+    if let repo = repo {
+      repo.save(ny)
+      repo.save(sf)
+    }
+  }
+
+  func testItFindsByName() {
+    if let repo = repo {
+      if let foundWhiteboard = repo.find(name: ny.name) {
+        XCTAssertEqual(foundWhiteboard, ny)
+      } else {
+        XCTFail()
+      }
+
+      if let foundWhiteboard = repo.find(name: sf.name) {
+        XCTAssertEqual(foundWhiteboard, sf)
+      } else {
+        XCTFail()
+      }
+    }
+  }
+
+  func testItCreatesUniqueIDsForWhiteboardsWhenSaved() {
+    if repo != nil {
+      XCTAssertNotNil(ny.id)
+      XCTAssertNotNil(sf.id)
+      XCTAssertNotEqual(ny.id, sf.id)
+    }
+  }
+
+  func testItFindsByID() {
+    if let repo = repo {
+      var foundWhiteboard = repo.find(id: ny.id!)!
+      XCTAssertEqual(foundWhiteboard, ny)
+
+      foundWhiteboard = repo.find(id: sf.id!)!
+      XCTAssertEqual(foundWhiteboard, sf)
+    }
+  }
+}
