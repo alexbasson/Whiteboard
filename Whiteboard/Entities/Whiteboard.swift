9fa1ec59975034eeeea8d9bf2b27535cd54763f1
Alex Basson <alexbasson@gmail.com>
2016-07-09 12:59:44 -0400

    Add enums for whiteboard error cases

--- Whiteboard/Entities/Whiteboard.swift
+++ Whiteboard/Entities/Whiteboard.swift
@@ -19,50 +19,50 @@ public func ==(lhs: Whiteboard, rhs: Whiteboard) -> Bool {
 }
 
 public enum WhiteboardField {
-    case Name
+  case Name
 }
 
 extension WhiteboardField: Equatable {}
 
 public func ==(lhs: WhiteboardField, rhs: WhiteboardField) -> Bool {
-    return true
+  return true
 }
 
 public enum ValidationConstraint {
-    case Required
-    case Unique
+  case Required
+  case Unique
 }
 
 extension ValidationConstraint: Equatable {}
 
 public func==(lhs: ValidationConstraint, rhs: ValidationConstraint) -> Bool {
-    switch (lhs, rhs) {
-    case (.Required, .Required):
-        return true
-    case (.Unique, .Unique):
-        return true
-    default:
-        return false
-    }
+  switch (lhs, rhs) {
+  case (.Required, .Required):
+    return true
+  case (.Unique, .Unique):
+    return true
+  default:
+    return false
+  }
 }
 
 public enum WhiteboardError: ErrorProtocol {
-    case Validation(field: WhiteboardField, validation: ValidationConstraint)
-    case Update
-    case Destroy
+  case Validation(field: WhiteboardField, validation: ValidationConstraint)
+  case Update
+  case Destroy
 }
 
 extension WhiteboardError: Equatable {}
 
 public func ==(lhs: WhiteboardError, rhs: WhiteboardError) -> Bool {
-    switch(lhs, rhs) {
-    case (let .Validation(field1, validation1), let .Validation(field2, validation2)):
-        return field1 == field2 && validation1 == validation2
-    case (.Update, .Update):
-        return true
-    case (.Destroy, .Destroy):
-        return true
-    default:
-        return false
-    }
+  switch(lhs, rhs) {
+  case (let .Validation(field1, validation1), let .Validation(field2, validation2)):
+    return field1 == field2 && validation1 == validation2
+  case (.Update, .Update):
+    return true
+  case (.Destroy, .Destroy):
+    return true
+  default:
+    return false
+  }
 }
